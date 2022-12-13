6e20e73bac7fe33e8f7774617b9070c2d7fc2ce0
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2022-12-13 13:16:32 -0300

    Projeto 3 e Modulo 4 do Bootcamp Finalizado

---
+++ Criando um Sistema e Abstraindo um Celular com POO em C#/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "(71) 9 9845-1256", modelo: "Nokia 5.4 128GB", imei: "977526421", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Nokia(numero: "(41) 9 9821-1629", modelo: "Iphone 11 64GB", imei: "728526497", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp 2");
